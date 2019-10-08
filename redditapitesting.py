7d4830dbbfab83578a586cd12b0c5be593b1f3fd
Manubilo <mjdelgado@pucp.pe>
2019-10-08 18:37:11 -0500

    initial code

---
+++ redditapitesting.py
@@ -0,0 +1,16 @@
+import praw
+import json
+
+client_id = "c6jR8eRWQ0RtaA"
+client_secret = "yQ55W5ku_3sah67xkZ_PIPaFrw4"
+user_agent = "testingAPI"
+username = "Manubilo"
+password = "werfault96"
+
+reddit = praw.Reddit(client_id=user_values['client_id'],
+client_secret=user_values['client_secret'],
+user_agent=user_values['user_agent'],
+username=user_values['username'],
+password=user_values['password'],)
+
+def create_reddit_object(json_file="reddit_config.json", json.key="reddit")
